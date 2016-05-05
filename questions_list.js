593e1244a9ff1a8f225e24b9e1773b4e54b8b0b4
Gregory Chang <gcchang2@illinois.edu>
2016-05-05 18:18:28 -0500

    questions son changed to js (so that it can be imported into page)

---
+++ questions_list.js
@@ -0,0 +1,184 @@
+var qlist = {
+    "questions_list": [{
+        "question": "Welfare recipients should be drug tested.",
+        "qtopic": "poverty",
+        "qid": 1,
+        "qhelp": "Info on issue"
+
+    }, {
+        "question": "Earned Income Tax Credit should be raised.",
+        "qtopic": "poverty",
+        "qid": 2,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Homeowners facing foreclosure should get help refinancing.",
+        "qtopic": "poverty",
+        "qid": 3,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "The government should take action against predatory lending practices.",
+        "qtopic": "poverty",
+        "qid": 4,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Do you support the idea of debt-free college?",
+        "qtopic": "education",
+        "qid": 5,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should Common Core standards be repealed?",
+        "qtopic": "education",
+        "qid": 6,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the government negotiate with teacher's unions to raise pay?",
+        "qtopic": "education",
+        "qid": 7,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should all college campuses implement affirmative consent standards for sexual assault?",
+        "qtopic": "education",
+        "qid": 8,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should Roe v Wade be overturned?",
+        "qtopic": "abortion",
+        "qid": 9,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should funding for Planned Parenthood be protected?",
+        "qtopic": "abortion",
+        "qid": 10,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the Hyde Amendment be rescinded",
+        "qtopic": "abortion",
+        "qid": 11,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should there be punishments for women who get abortions?",
+        "qtopic": "abortion",
+        "qid": 12,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the death penalty be reinstated for some crimes?",
+        "qtopic": "death penalty",
+        "qid": 13,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Are the current methods of execution humane?",
+        "qtopic": "death penalty",
+        "qid": 14,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Is the death penalty to expensive in it's current form?",
+        "qtopic": "death penalty",
+        "qid": 15,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the death penalty be a state or federal issue?",
+        "qtopic": "death penalty",
+        "qid": 16,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the army carpet bomb the Middle-East?",
+        "qtopic": "security",
+        "qid": 17,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the US enter a coalition to combat ISIS/ISIL?",
+        "qtopic": "security",
+        "qid": 18,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the army get involved in Syria?",
+        "qtopic": "security",
+        "qid": 19,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should illegal interrogation practices at facilities like Guantanamo Bay be encouraged?",
+        "qtopic": "security",
+        "qid": 20,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should healthcare be considered a human right?",
+        "qtopic": "healthcare",
+        "qid": 21,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the government combat profiteering by pharmaceutical companies?",
+        "qtopic": "healthcare",
+        "qid": 22,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should Obamacare be repealed?",
+        "qtopic": "healthcare",
+        "qid": 23,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should health insurance monopolies be broken?",
+        "qtopic": "healthcare",
+        "qid": 24,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Is climate change real?",
+        "qtopic": "climate change",
+        "qid": 25,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should America make commitments pertaining to the Paris climate deal?",
+        "qtopic": "climate change",
+        "qid": 26,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the Keystone XL pipeline be built?",
+        "qtopic": "climate change",
+        "qid": 27,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should fracking be allowed?",
+        "qtopic": "climate change",
+        "qid": 28,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should Obama's executive orders on gun sales be repealed?",
+        "qtopic": "gun control",
+        "qid": 29,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should background checks on gun purchases be expanded?",
+        "qtopic": "gun control",
+        "qid": 30,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the goverment have the right to institute gun-free zones?",
+        "qtopic": "gun control",
+        "qid": 31,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the sale of assualt rifles be banned?",
+        "qtopic": "gun control",
+        "qid": 32,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should existing illegal immigrants be granted a path to citizenship?",
+        "qtopic": "immigration",
+        "qid": 33,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should we build a wall?",
+        "qtopic": "immigration",
+        "qid": 34,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should the goverment expend resources on deportation raids?",
+        "qtopic": "immigration",
+        "qid": 35,
+        "qhelp": "Info on issue"
+    }, {
+        "question": "Should refugees fleeing from violence be accepted into the US?",
+        "qtopic": "immigration",
+        "qid": 36,
+        "qhelp": "Info on issue"
+    }]
+}
