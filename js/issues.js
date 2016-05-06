096fea3b7129af377dbb8064237dfc8ad880140c
Gregory Chang <gcchang2@illinois.edu>
2016-05-06 15:50:38 -0500

    new issues.js

---
+++ js/issues.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    var answer_array = sessionStorage.getItem('myArray');
+    //alert(answer_array);
+
+    //Colbert overall percetage
+    var overall_num1 = -1;
+
+    //Santorum overall percentage
+    var overall_num2 = -1;
+
+    //Issue percentages
+    var poverty_num1 = -1;
+    var education_num1 = -1;
+    var abortion_num1 = -1;
+    var deathpenalty_num1 = -1;
+    var security_num1 = -1;
+    var healthcare_num1 = -1;
+    var climatechange_num1 = -1;
+    var immigration_num1 = -1;
+    var guncontrol_num1 = -1;
+    
+    var poverty_num2 = -1;
+    var education_num2 = -1;
+    var abortion_num2 = -1;
+    var deathpenalty_num2 = -1;
+    var security_num2 = -1;
+    var healthcare_num2 = -1;
+    var climatechange_num2 = -1;
+    var immigration_num2 = -1;
+    var guncontrol_num2 = -1;
+
+
+});
