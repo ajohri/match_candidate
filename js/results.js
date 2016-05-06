2a792005d72587ceec56309cc8eb533734de612d
Gregory Chang <gcchang2@illinois.edu>
2016-05-06 13:24:56 -0500

    whole bunch of changes
    cleaned up url params passed from importance.html to questions.html
    can now hide/show a topic's questions by clicking glyphicon
    only questions that were rated in importance.html are shown in
    questions.html

---
+++ js/results.js
@@ -0,0 +1,32 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var categories = getParameterByName('abortion');
+
+$(document).ready(function() {
+
+
+
+
+    if (categories != null) {
+
+        var i;
+        for (i = 0; i <= 5; i++) {
+            if (categories.indexOf(i) >= 0) {
+                // document.getElementById(i).style.display = "inline";
+            }
+        }
+    } else {
+        var i;
+        for (i = 0; i <= 5; i++) {
+            // document.getElementById(i).style.display = "inline";
+        }
+    }
+});
