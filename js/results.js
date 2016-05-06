6ef9dadd611a2630bfe10e2ca0aca80e5763ea33
Gregory Chang <gcchang2@illinois.edu>
2016-05-06 15:07:29 -0500

    quick update

--- js/results.js
+++ js/results.js
@@ -22,15 +22,25 @@ $(document).ready(function() {
     var answer_array = sessionStorage.getItem('myArray');
     //alert(answer_array);
 
-    var poverty_num = -1;
-    var education_num = -1;
-    var abortion_num;
-    var deathpenalty_num;
-    var security_num;
-    var healthcare_num;
-    var climatechange_num;
-    var immigration_num;
-    var guncontrol_num;
+    var poverty_num1 = -1;
+    var education_num1 = -1;
+    var abortion_num1 = -1;
+    var deathpenalty_num1 = -1;
+    var security_num1 = -1;
+    var healthcare_num1 = -1;
+    var climatechange_num1 = -1;
+    var immigration_num1 = -1;
+    var guncontrol_num1 = -1;
+    
+    var poverty_num2 = -1;
+    var education_num2 = -1;
+    var abortion_num2 = -1;
+    var deathpenalty_num2 = -1;
+    var security_num2 = -1;
+    var healthcare_num2 = -1;
+    var climatechange_num2 = -1;
+    var immigration_num2 = -1;
+    var guncontrol_num2 = -1;
 
     for (var i = 1; i < 36; i++) {
         if(answer_array[i] != null) {
