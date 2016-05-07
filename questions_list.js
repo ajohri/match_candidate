b2123df87609706af8c4eb88f1a8d6160cb25d71
Gregory Chang <gcchang2@illinois.edu>
2016-05-06 23:36:11 -0500

    questions now statements

--- questions_list.js
+++ questions_list.js
@@ -21,162 +21,162 @@ var qlist = {
         "qid": 4,
         "qhelp": "Info on issue"
     }, {
-        "question": "Do you support the idea of debt-free college?",
+        "question": "Debt-free college.",
         "qtopic": "education",
         "qid": 5,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should Common Core standards be repealed?",
+        "question": "Common Core standards should be repealed.",
         "qtopic": "education",
         "qid": 6,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the government negotiate with teacher's unions to raise pay?",
+        "question": "The government should negotiate with teacher's unions to raise pay.",
         "qtopic": "education",
         "qid": 7,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should all college campuses implement affirmative consent standards for sexual assault?",
+        "question": "All college campuses should implement affirmative consent standards for sexual assault.",
         "qtopic": "education",
         "qid": 8,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should Roe v Wade be overturned?",
+        "question": "Roe v Wade should be overturned?",
         "qtopic": "abortion",
         "qid": 9,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should funding for Planned Parenthood be protected?",
+        "question": "Funding for Planned Parenthood should be protected.",
         "qtopic": "abortion",
         "qid": 10,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the Hyde Amendment be rescinded",
+        "question": "Hyde Amendment be rescinded",
         "qtopic": "abortion",
         "qid": 11,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should there be punishments for women who get abortions?",
+        "question": "There should be punishments for women who get abortions.",
         "qtopic": "abortion",
         "qid": 12,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the death penalty be reinstated for some crimes?",
+        "question": "The death penalty should be reinstated for some crimes.",
         "qtopic": "death penalty",
         "qid": 13,
         "qhelp": "Info on issue"
     }, {
-        "question": "Are the current methods of execution humane?",
+        "question": "The current methods of execution are humane.",
         "qtopic": "death penalty",
         "qid": 14,
         "qhelp": "Info on issue"
     }, {
-        "question": "Is the death penalty to expensive in it's current form?",
+        "question": "The death penalty is too expensive in it's current form.",
         "qtopic": "death penalty",
         "qid": 15,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the death penalty be a state or federal issue?",
+        "question": "The death penalty is federal issue.",
         "qtopic": "death penalty",
         "qid": 16,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the army carpet bomb the Middle-East?",
+        "question": "Th army should carpet bomb the Middle-East.",
         "qtopic": "security",
         "qid": 17,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the US enter a coalition to combat ISIS/ISIL?",
+        "question": "The US should enter a coalition to combat ISIS/ISIL.",
         "qtopic": "security",
         "qid": 18,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the army get involved in Syria?",
+        "question": "The army should get involved in Syria.",
         "qtopic": "security",
         "qid": 19,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should illegal interrogation practices at facilities like Guantanamo Bay be encouraged?",
+        "question": "Illegal interrogation practices at facilities like Guantanamo Bay should be encouraged.",
         "qtopic": "security",
         "qid": 20,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should healthcare be considered a human right?",
+        "question": "Healthcare should be considered a human right.",
         "qtopic": "healthcare",
         "qid": 21,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the government combat profiteering by pharmaceutical companies?",
+        "question": "The government should combat profiteering by pharmaceutical companies.",
         "qtopic": "healthcare",
         "qid": 22,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should Obamacare be repealed?",
+        "question": "Obamacare should be repealed.",
         "qtopic": "healthcare",
         "qid": 23,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should health insurance monopolies be broken?",
+        "question": "Health insurance monopolies should be broken.",
         "qtopic": "healthcare",
         "qid": 24,
         "qhelp": "Info on issue"
     }, {
-        "question": "Is climate change real?",
+        "question": "Climate change is real.",
         "qtopic": "climate change",
         "qid": 25,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should America make commitments pertaining to the Paris climate deal?",
+        "question": "America should make commitments pertaining to the Paris climate deal.",
         "qtopic": "climate change",
         "qid": 26,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the Keystone XL pipeline be built?",
+        "question": "The Keystone XL pipeline should be built.",
         "qtopic": "climate change",
         "qid": 27,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should fracking be allowed?",
+        "question": "Fracking should be allowed.",
         "qtopic": "climate change",
         "qid": 28,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should Obama's executive orders on gun sales be repealed?",
+        "question": "Obama's executive orders on gun sales should be repealed.",
         "qtopic": "gun control",
         "qid": 33,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should background checks on gun purchases be expanded?",
+        "question": "Background checks on gun purchases should be expanded.",
         "qtopic": "gun control",
         "qid": 34,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the goverment have the right to institute gun-free zones?",
+        "question": "The goverment should have the right to institute gun-free zones.",
         "qtopic": "gun control",
         "qid": 35,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the sale of assualt rifles be banned?",
+        "question": "The sale of assault rifles should be banned.",
         "qtopic": "gun control",
         "qid": 36,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should existing illegal immigrants be granted a path to citizenship?",
+        "question": "Existing illegal immigrants should be granted a path to citizenship.",
         "qtopic": "immigration",
         "qid": 29,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should we build a wall?",
+        "question": "We build a wall on the southern border.",
         "qtopic": "immigration",
         "qid": 30,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should the goverment expend resources on deportation raids?",
+        "question": "The goverment should expend resources on deportation raids.",
         "qtopic": "immigration",
         "qid": 31,
         "qhelp": "Info on issue"
     }, {
-        "question": "Should refugees fleeing from violence be accepted into the US?",
+        "question": "Refugees fleeing from violence should be accepted into the US.",
         "qtopic": "immigration",
         "qid": 32,
         "qhelp": "Info on issue"
